b0d3a67dc0b24e07ba61c57b092d094c78616ca3
Shermoncher <trubacheva.daria@yandex.ru>
2020-05-25 21:29:06 +0300

    lesson_2

---
+++ lessons/lesson_2/main.py
@@ -0,0 +1,16 @@
+a = (1, 2, 3,"abc")
+print (a)
+d = {}
+print(d)
+d["вася"] = 34
+d["оля"] = 23
+print(d["оля"])
+
+
+x = 2
+if x % 2 == 0:
+    print("четные")
+elif x % 3 == 0:
+    print(" /на 3 ")
+else:
+    print ("хрень")
